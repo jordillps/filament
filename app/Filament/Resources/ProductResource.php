76054949d3bd16e6942f2b33326c02c8d94ea2f6
Jordi Llobet <jordillps@gmail.com>
2022-12-20 16:18:48 +0100

    Product Resource

---
+++ app/Filament/Resources/ProductResource.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProductResource\Pages;
+use App\Filament\Resources\ProductResource\RelationManagers;
+use App\Models\Product;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\Card;
+use Filament\Forms\Components\Select;
+use Livewire\TemporaryUploadedFile;
+use Filament\Tables\Columns\Layout\Split;
+
+class ProductResource extends Resource
+{
+    protected static ?string $model = Product::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-shopping-bag';
+
+    protected static ?string $modelLabel = 'producto';
+
+    protected static ?string $pluralModelLabel = 'productos';
+
+    protected static ?string $navigationGroup = 'Gestión';
+
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Card::make()
+                ->schema([
+                Forms\Components\TextInput::make('name')->required()->maxLength(255),
+                Forms\Components\Textarea::make('description')->required()->maxLength(65535),
+                Select::make('processes')
+                            ->multiple()
+                            ->relationship('processes', 'name'),
+                Forms\Components\TextInput::make('price')->type('number')->step('any')
+                    ->required(),
+                Forms\Components\FileUpload::make('image')->image()
+                    ->disk('products-images')
+                    ->getUploadedFileNameForStorageUsing(function (TemporaryUploadedFile $file): string {
+                        return (string) str($file->getClientOriginalName())->prepend('product-'. date('d-m-Y-H-i-s-'));
+                    })
+                ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                // Split::make([
+                    Tables\Columns\TextColumn::make('id')->sortable()->searchable(),
+                    Tables\Columns\ImageColumn::make('image')->disk('products-images'),
+                    Tables\Columns\TextColumn::make('name')->sortable()->searchable(),
+                    Tables\Columns\TextColumn::make('price')->money('eur')->extraAttributes(['class' => 'text-right']),
+                    Tables\Columns\TextColumn::make('created_at')
+                        ->date('d-m-Y'),
+                    // ])
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProducts::route('/'),
+            'create' => Pages\CreateProduct::route('/create'),
+            'edit' => Pages\EditProduct::route('/{record}/edit'),
+        ];
+    }    
+}
--- config/filesystems.php
+++ config/filesystems.php
@@ -44,6 +44,20 @@ return [
             'throw' => false,
         ],
 
+        'products-images' => [
+            'driver' => 'local',
+            'root' => public_path('/img/products'),
+            'url' => env('APP_URL').'/img/products',
+            'visibility' => 'public',
+        ],
+
+        'users-images' => [
+            'driver' => 'local',
+            'root' => public_path('/img/users'),
+            'url' => env('APP_URL').'/img/users',
+            'visibility' => 'public',
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
