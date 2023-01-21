9987a7f6b30b696e1ce8bdddca30919d4f0d15e4
Jordi Llobet <jordillps@gmail.com>
2023-01-21 11:50:37 +0100

    Esborrar imatge al actualitzar usuaris,prodcutes

--- app/Filament/Resources/ProductResource/Pages/EditProduct.php
+++ app/Filament/Resources/ProductResource/Pages/EditProduct.php
@@ -6,6 +6,7 @@ use App\Filament\Resources\ProductResource;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\EditRecord;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\File; 
 
 class EditProduct extends EditRecord
 {
@@ -19,6 +20,12 @@ class EditProduct extends EditRecord
     }
 
     protected function handleRecordUpdate(Model $record, array $data): Model{ 
+        $fileCurrentImagePath = public_path(). '/img/products/' .$record->photo_path;
+        if($record->photo_path != $data['photo_path']){
+           if(File::exists($fileCurrentImagePath)){ 
+                File::delete($fileCurrentImagePath);
+            }
+        }
         $record->update($data);
         $record->updateProductPrice();
         return $record;
--- app/Filament/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Resources/UserResource/Pages/EditUser.php
@@ -5,17 +5,19 @@ namespace App\Filament\Resources\UserResource\Pages;
 use App\Filament\Resources\UserResource;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Database\Eloquent\Model;
 use Filament\Forms;
 use Filament\Resources\Form;
 use Illuminate\Validation\Rules\Password;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\File; 
+
 
 class EditUser extends EditRecord
 {
     protected static string $resource = UserResource::class;
 
-    protected function getActions(): array
-    {
+    protected function getActions(): array{
         return [
             // Actions\DeleteAction::make(),
 
@@ -41,5 +43,16 @@ class EditUser extends EditRecord
             //     $this->notify('success', 'Password updated successfully');
             // }),
         ];
+    }
+
+    protected function handleRecordUpdate(Model $record, array $data): Model{ 
+        $fileCurrentImagePath = public_path(). '/img/users/' .$record->photo_path;
+        if($record->photo_path != $data['photo_path']){
+           if(File::exists($fileCurrentImagePath)){ 
+                File::delete($fileCurrentImagePath);
+            }
+        }
+        $record->update($data);
+        return $record;
     }
 }
